d1cad27b73dba38fa114144647f1f55b125a6fe2
NLewe <Natascha.Lewe@vuw.ac.nz>
2023-03-23 07:35:33 +1300

    updated bayes models, included script for PCs as variables corrected upload for data

--- scripts/1_dataNL_prep.R
+++ scripts/1_dataNL_prep.R
@@ -15,22 +15,13 @@ meta_plants <- read_rds ("data/meta_plants.rds")
 meta_M1Wcontrol <-  read_rds ("data/meta_M1Wcontrol.rds")
 
 dataNLrootstosoil <- read_rds ("data/dataNLrootstosoil.rds")# NLFA_data for exp 2 #
-All_metrics_M1_8Sp <- read_rds ("data/All_metrics_M1_8Sp.rds")# mean metrics for exp 2 interaction niches #
 Metrics_exp2 <- read_rds("data/Metrics_exp2.rds")  # samplewise results of interaction niche properties #
 
-PC_data_M1roots  <-  read_rds ("data/PC_data_M1roots.rds")# results from PCA of interaction niche properties #
-
+dataNL <- read_rds ("data/dataNL.rds")
 A <- read_rds ("data/covariance_str_tree_trnl_plants.rds") #covaraince structure based on trnl tree , for glmm models
 
 
-# data 
-dataNL <- 
-  dataNLrootstosoil %>%  
-  select (sampleID, AMF, AMFroot, PlaSpe, 
-          PlantSpeciesfull, DW_above, DW_roots, totalAMF, # both AMF and AMFroot are in µmol per g DW soil or roots
-          Dim.1, Dim.2, Dim.3) %>% 
-  left_join (Metrics_exp2) %>% 
-  mutate (AMF = AMF*1000)
+
 
 
 

420283387a35890221a1df746935935d5bb28618
NLewe <Natascha.Lewe@vuw.ac.nz>
2023-05-29 16:42:00 +1200

    added calculations for supplementary tables

--- scripts/1B_dataNL_PL_prep.R
+++ scripts/1B_dataNL_PL_prep.R
@@ -71,4 +71,100 @@ adiv_richness_M1 %>%  select (-PlaSpe, -PlantFamily) %>%
   mutate (Shannon = round (Shannon, 3)) %>% 
   write_excel_csv("results/tableS6_alpha_div_E2.csv")
 
+# Table S  dada resuls ####
+
+dada <- read_csv("results/track_reads_M0_PP.csv")
+
+
+dada_E1_8Sp <- meta_M1 %>%  select (PlaSpe) %>% unique  () %>% left_join (dada, by = "PlaSpe")  %>%
+  tibble () %>%  select (-sortedPlantSpecies, - PlantFamily, -PlantType, -value)
+
+# all reads ##
+
+dada_E1_8Sp %>%  summarize (sum (input))
+
+dada_E1_8Sp %>%  summarize (sum (nonchim))
+
+
+
+## get number of ASVs,  etc  ##
+
+ps_ALL_E1 <- readRDS("data/ps_ALL_E1.rds") ## needs ps of experiment 1 
+
+ps_ALL_E1_8Sp <- subset_samples (ps_ALL_E1, PlaSpe == "AchMil"| PlaSpe == "CicInt" |
+                                   PlaSpe == "SchAru"| PlaSpe == "PoaCit"|
+                                   PlaSpe == "PlaLan"| PlaSpe == "HolLan"|
+                                   PlaSpe == "BroWil"| PlaSpe == "AgrCap") # filterd for the plants f interest
+ps_ALL_E1_8Sp  <-  prune_taxa (taxa_sums(ps_ALL_E1_8Sp)>=1, ps_ALL_E1_8Sp) ## ASVs removed that are not found in those 8 species
+
+## 
+estimate_richness(ps_ALL_E1_8Sp) %>% summarize (n = mean (Observed),sd = sd (Observed))  # to get average ASV per smalple
+estimate_richness(ps_ALL_E1_8Sp) %>%  arrange( Observed)
+
+# `rarefaction-ALL, fig.cap = "A) Rarefaction (solid line segments) and extrapolation (dotted line segments) 
+#sampling curves of fungal ASV richness for each plant species and soil. B) Sample completeness curves by sequence reads. c) Sample coverage curves. Shaded areas in each plot show the 95% confidence interval of the extrapolation. ", fig.height=8, fig.width=12, dpi = 300 }
+#agglomerate to Plant Species
+ps_ALL_E1_8Sp_perPlaSpe  <- merge_samples(ps_ALL_E1_8Sp, group = "PlantSpeciesfull")
+
+#make df for Glo_PlaSpe data (empty samples samples pruned )
+comm_df_ALL_E1_8Sp_PlaSpe  <- data.frame (otu_table (ps_ALL_E1_8Sp_perPlaSpe))### needs ps with all asvs only 8plants)) 
+
+#rarefaction in iNEXT 
+iNext_ALL_E1_8_sp_PlaSpe_richness <- iNEXT (t (comm_df_ALL_E1_8Sp_PlaSpe), 
+                                    q= 0, # richness
+                                    datatype =  "abundance", 
+                                    endpoint = NULL, 
+                                    knots = 400 )
+# plot 
+p1 <- ggiNEXT(iNext_ALL_E1_8_sp_PlaSpe_richness, type = 1) +
+  theme_classic() + 
+  scale_shape_manual(values = c(0,1, 2,3,4,5,6,7)) + 
+  scale_y_continuous (name = "ASV richness") +
+  scale_x_continuous(name = "Number of sequence reads")+
+  theme (axis.text = element_text(size =14), 
+         axis.title = element_text(size =18),
+         legend.text = element_text(face = "italic"))+
+  guides(     colour=guide_legend(title="Plant species"), 
+              fill=guide_legend(title="Plant species"), 
+              shape=guide_legend(title="Plant species"))
+
+
+p2 <- ggiNEXT(iNext_ALL_E1_8_sp_PlaSpe_richness, type = 2) +
+  theme_classic() + 
+  scale_shape_manual(values = c(0,1, 2,3,4,5,6,7)) + 
+  scale_x_continuous(name = "Number of sequence reads")+
+  theme (axis.text = element_text(size =14), 
+         axis.title = element_text(size =18), 
+         legend.text = element_text(face = "italic"))+
+  guides(     colour=guide_legend(title="Plant species"), 
+              fill=guide_legend(title="Plant species"), 
+              shape=guide_legend(title="Plant species"))
+
+p3 <- ggiNEXT(iNext_ALL_E1_8_sp_PlaSpe_richness, type = 3) +
+  theme_classic() + 
+  scale_shape_manual(values = c(0,1, 2,3,4,5,6,7)) + 
+  scale_y_continuous(name = "ASV richness")+
+  scale_x_continuous (labels = scales::percent) + 
+  theme (axis.text = element_text(size =14), 
+         axis.title = element_text(size =18),  
+         legend.text = element_text(face = "italic"))+
+  guides(     colour=guide_legend(title="Plant species"), 
+              fill=guide_legend(title="Plant species"), 
+              shape=guide_legend(title="Plant species"))
+
+ggarrange (p1,p2,p3, ncol = 3, common.legend = T, labels = "AUTO", legend = "bottom")
+
+### AMF families description ####
+Glo_Fam_perc_reads_E1_8Sp  <- 
+  ASV_table_Glo  %>%  
+  select (-c(Kingdom, Phylum, Class, Order, Genus, Species))  %>%  
+  pivot_longer (!c(Family, ASV_ID), names_to = "sampleID", values_to="counts")  %>%
+  group_by (Family)  %>% 
+  summarize(Fam_sum=sum(counts)) %>% 
+  add_column (Glo_total_counts)  %>% 
+  mutate (Glo_Fam_perc = 100  * Fam_sum/total)  %>% 
+  arrange(desc(Fam_sum))  %>% 
+  as.data.frame()
+
+
                                 

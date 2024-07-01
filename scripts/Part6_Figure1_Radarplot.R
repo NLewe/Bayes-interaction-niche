7a8456c6f380937d5a5238e4870304b25a6e67c1
NLewe <Natascha.Lewe@vuw.ac.nz>
2024-07-01 17:48:52 +1200

    Code cleanup for resubmission

--- scripts/Part6_Figure1_Radarplot.R
+++ scripts/Part6_Figure1_Radarplot.R
@@ -1,7 +1,7 @@
-### Part 6: Figure 1: Radar of species' interaction generalism ####
+### Part 6: Figure 1: Radarplots: species' interaction generalism ####
 
 
-## packages ####
+## Packages ####
 library (tidyverse)
 library (ggradar)
 library (ggpubr)
@@ -11,37 +11,35 @@ library (flextable)
 library (RVAideMemoire)
 
 
-## radar plots per experiment (not shown) ####
+## Radar plots per experiment (not shown in publication) ####
 
-plot_radar_E1<- 
-  All_metrics_E1 %>%  
-  dplyr::select (-PlantFamily, -Exp) %>% 
-  ungroup() %>% 
-  mutate(across(!c(PlantSpeciesfull), rescale)) %>% 
-  ggradar(legend.title = "Plant species", 
-          plot.title = "Experiment 1")
+# plot_radar_E1<- 
+#   All_metrics_E1 %>%  
+#   dplyr::select (-PlantFamily, -Exp) %>% 
+#   ungroup() %>% 
+#   mutate(across(!c(PlantSpeciesfull), rescale)) %>% 
+#   ggradar(legend.title = "Plant species", 
+#           plot.title = "Experiment 1")
+# 
+# plot_radar_E2 <- 
+#   All_metrics_E2 %>%  ungroup () %>% 
+#   dplyr::select (-PlantFamily, -Exp) %>% 
+#   mutate(across(!c(PlantSpeciesfull), rescale)) %>% # all metrics get scaled to obtain their relative values to each other, 
+#   #i.e. from most specialist to most generalist
+#   ggradar(legend.title = "Plant species", plot.title = "Experiment 2", 
+#           background.circle.colour = "white",
+#           background.circle.transparency = 0, 
+#           axis.line.colour = "grey30", 
+#           gridline.min.colour = "grey30",
+#           gridline.mid.colour = "grey30",
+#           gridline.max.colour = "grey30") 
+# 
+# 
+# ggarrange (plot_radar_E1, plot_radar_E2, common.legend = T, legend = "bottom", label = "AUTO")
 
-plot_radar_E2 <- 
-  All_metrics_E2 %>%  ungroup () %>% 
-  dplyr::select (-PlantFamily, -Exp) %>% 
-  mutate(across(!c(PlantSpeciesfull), rescale)) %>% # all metrics get scaled to obtain their relative values to each other, 
-  #i.e. from most specialist to most generalist
-  ggradar(legend.title = "Plant species", plot.title = "Experiment 2", 
-          background.circle.colour = "white",
-          background.circle.transparency = 0, 
-          axis.line.colour = "grey30", 
-          gridline.min.colour = "grey30",
-          gridline.mid.colour = "grey30",
-          gridline.max.colour = "grey30") 
 
-
-
-
-
-ggarrange (plot_radar_E1, plot_radar_E2, common.legend = T, legend = "bottom", label = "AUTO")
-
-# Prepare an empty radarplot for the figure ##
-extra <- tribble (~PlantSpeciesfull,~Richness, ~Shannon, ~MPD, ~y_MPD, ~uniqueASV, ~CU, ~`β(core)`,  "leer", 0, 0, 0, 0, 0, 0, 0 )
+# Empty radarplot to include in Figure 1 ####
+extra <- tribble (~PlantSpeciesfull,~Richness, ~Shannon, ~MPD, ~y_MPD, ~uniqueASV, ~CU, ~`β(core)`, ~Unifrac, "leer", 0, 0, 0, 0, 0, 0, 0,0)
 radar_empty  <- ggradar(extra,          background.circle.colour = "white",
                         background.circle.transparency = 0, 
                         axis.line.colour = "grey30", 
@@ -61,7 +59,7 @@ radar_plots <-
                ungroup() %>% 
                mutate(across(!c(PlantSpeciesfull,PlantFamily,Exp ), rescale))) %>% 
   dplyr:: select (!PlantFamily) %>% 
-  select (PlantSpeciesfull, `Richness S`, `Shannon's H'`, MPD, `Phyl. γ-diversity`, ,`γ-diversity`, `β(CU)` ,`β(core)`, Exp ) %>% 
+  select (PlantSpeciesfull, `Richness S`, `Shannon's H'`, MPD, `Phyl. γ-diversity`, ,`γ-diversity`, `β(CU)` ,`β(core)`, Unifrac, Exp ) %>% 
     group_split(PlantSpeciesfull) %>% 
   map (~dplyr::select (., -PlantSpeciesfull) %>% relocate (Exp)) %>% 
   map (~ggradar(.,          background.circle.colour = "white",
@@ -71,7 +69,7 @@ radar_plots <-
                 gridline.mid.colour = "grey30",
                 gridline.max.colour = "grey30",
                 values.radar = c(" ", " ", " "),
-                axis.labels = c("", "","", " ", "", "", "")))
+                axis.labels = c("", "","", " ", "", "", "", ""))) # remove labels
 
 ## Arrange the plots ##
 ggarrange (NULL, radar_plots[[1]], radar_plots[[2]], radar_plots[[3]], radar_plots[[4]], 
@@ -81,46 +79,46 @@ ggarrange (NULL, radar_plots[[1]], radar_plots[[2]], radar_plots[[3]], radar_plo
            font.label = list (face = "italic"),
            common.legend = T, nrow = 3, ncol=3)
 
-# Further labelling for the plot was added manually using inkscape
+ggsave ("figures/testFig1.svg", width = 18, height = 18)
+
+# Note: Further formatting of Figure 1 was applied manually using inkscape.
 
 
-# sorted for generalism according to table below (not shown) 
-ggarrange (radar_plots[[5]], radar_plots[[8]], radar_plots[[7]], radar_plots[[1]], 
-           radar_plots[[3]], radar_plots[[6]], radar_plots[[2]], radar_plots[[4]], 
-           labels = c("H. lanatus", "S. arundinaceus", "P. lanceolata","A. capillaris",
-                      "B. willdenowii","P. cita", "A. millefolium",  "C. intybus" 
-           ),
-           common.legend = T, nrow = 2, ncol=4)
-
-
-
-
-library (FactoMineR)
-library(factoextra)
-library(ggrepel)
-library (ggpubr)
-library(vegan)
+# # sorted for generalism according to table below (not shown) 
+# ggarrange (radar_plots[[5]], radar_plots[[8]], radar_plots[[7]], radar_plots[[1]], 
+#            radar_plots[[3]], radar_plots[[6]], radar_plots[[2]], radar_plots[[4]], 
+#            labels = c("H. lanatus", "S. arundinaceus", "P. lanceolata","A. capillaris",
+#                       "B. willdenowii","P. cita", "A. millefolium",  "C. intybus" 
+#            ),
+#            common.legend = T, nrow = 2, ncol=4)
 
 
 
 ## Procrustes analysis of the absolute values of the diversity metrics  #####
+
+## Packages ####
+library(vegan)
+
+
+# Permutational Procrustes test #####
 procr_test<- 
   All_metrics_E1 %>%  select (-Exp, -PlantFamily) %>% 
   pivot_longer(!PlantSpeciesfull, names_to = "metric", values_to = "Mvalue") %>%  
   left_join(All_metrics_E2 %>%  ungroup () %>% select (-Exp, -PlantFamily) %>% 
-              #mutate_at (vars (-PlantSpeciesfull), rescale) %>% 
               pivot_longer(!PlantSpeciesfull, names_to = "metric", values_to = "valuesE2")) %>%
   split(~PlantSpeciesfull) %>% 
-  map (~protest(X= .$Mvalue, Y = .$valuesE2, scale = F, scores = "sites", permutations = 9999, symmetric = F))
+  map (~protest(X= .$Mvalue, Y = .$valuesE2, scale = F, scores = "sites", 
+                permutations = 9999, symmetric = F))
 
 
-# randomisation (permutational) test estimate the significance of an observed statistic
+# Randomisation, i.e. (permutational) test estimate the significance of an observed statistic
 # relative to a large number of values for the same statistic that are generataed 
 # by permuting the original data #
-# get results in table #
+# 
+# Save a table with the results #
 map_dfr(procr_test, ~unlist (c(.$ss, .$svd$d, .$signif)) ) %>% 
   add_column ("procr" = c("M2", "correl", "sig")) %>%  
   mutate (across(where (is.numeric), ~round(.x, digits = 5))) %>% 
-  write.csv("results/Procrustes_test.csv")
+  write.csv("results/20240501_Procrustes_test.csv")
 
 

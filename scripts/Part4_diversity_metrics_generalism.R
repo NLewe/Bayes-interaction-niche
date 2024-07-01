f98f1dcc6cf93c98ffd5dba0fa4fa59903c966d2
NLewe <Natascha.Lewe@vuw.ac.nz>
2024-07-01 12:35:11 +1200

    changed the calculation of beta core in part4, added unifrac, added beta core for supplementary material

--- scripts/Part4_diversity_metrics_generalism.R
+++ scripts/Part4_diversity_metrics_generalism.R
@@ -9,11 +9,14 @@ library (picante)
 library (readxl)
 library (rstatix)
 library(ggpubr)
+library (phylosmith)
+library(metagMisc)
 
 # Load data #####
 # These objects include only the data for AMF , all other fungal sequences have been removed##
-ps_E1 <- read_rds ("data/ps_E1.rds")
-ps_E2 <- read_rds ("data/ps_E2.rds") ##
+ps_E1 <- read_rds ("data/ps_E1.rds")  %>% prune_taxa( taxa_sums (.)>0, .) %>% prune_samples(sample_sums (.) > 0, .)
+ps_E2 <- read_rds ("data/ps_E2.rds") %>% prune_taxa( taxa_sums (.)>0, .) %>% prune_samples(sample_sums (.) > 0, .) ##
+
 
 
 # Load meta tables
@@ -120,20 +123,122 @@ comp_units   <- meannumberASVsper_species  %>%
 #ß core ####
 
 # taxa core
-ps_Glo_core  <- phylosmith::taxa_core (ps_E1, treatment = "PlaSpe", frequency =0.6)
-ps_core_PlaSpe  <- merge_samples(ps_Glo_core, group = "PlantSpeciesfull", fun = sum)
+#ps_Glo_core  <- taxa_core (ps_E1, treatment = "PlaSpe", frequency =0.1)
+
+#Test step by step#
+#
+
+data_E1_melted <- melt_phyloseq(ps_E1)
+
+ASVs_in_1_samples <-
+  data_E1_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>% 
+  left_join (replicates_samples_after_Glo) %>% 
+  filter (n/repl>=0.2) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+
+ASVs_in_2_samples <-
+  data_E1_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>% 
+  left_join (replicates_samples_after_Glo) %>% 
+  filter (n/repl>=0.4) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+
+ASVs_in_3_samples <-
+data_E1_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>%   
+  left_join (replicates_samples_after_Glo) %>% 
+  filter (n/repl>=0.6) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+ASVs_in_4_samples <-
+  data_E1_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>% 
+  left_join (replicates_samples_after_Glo) %>% 
+  filter (n/repl>=0.8) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+ASVs_in_5_samples <-
+  data_E1_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>%   
+  left_join (replicates_samples_after_Glo) %>% 
+  filter (n/repl>=1) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+beta_core_E1_1 <- 
+  uniqueASVsperPlaSpe %>% 
+  left_join (ASVs_in_1_samples) %>% 
+  mutate (beta_core1 = round (1-(n/uniqueASVsperPlSpe),2))
+
+beta_core_E1_2 <- 
+  uniqueASVsperPlaSpe %>% 
+  left_join (ASVs_in_2_samples) %>% 
+  mutate (beta_core2 =round ( 1-(n/uniqueASVsperPlSpe), 2))
+
+beta_core_E1_3 <- 
+  uniqueASVsperPlaSpe %>% 
+  left_join (ASVs_in_3_samples) %>% 
+  mutate (beta_core3 = round (1-(n/uniqueASVsperPlSpe), 2))
+
+beta_core_E1_4 <- 
+  uniqueASVsperPlaSpe %>% 
+  left_join (ASVs_in_4_samples) %>% 
+  mutate (beta_core4 = round (1-(n/uniqueASVsperPlSpe),2))
+
+beta_core_E1_5 <- 
+  uniqueASVsperPlaSpe %>% 
+  left_join (ASVs_in_5_samples) %>% 
+  mutate (beta_core5 = round (1-(n/uniqueASVsperPlSpe),2))
+
+beta_core_E1_1 %>% 
+  left_join(beta_core_E1_2 %>%  select ( PlantSpeciesfull, beta_core2)) %>% 
+  left_join(beta_core_E1_3 %>%  select ( PlantSpeciesfull, beta_core3 )) %>% 
+  left_join(beta_core_E1_4 %>%  select ( PlantSpeciesfull, beta_core4)) %>% 
+  left_join(beta_core_E1_5 %>%  select ( PlantSpeciesfull, beta_core5)) %>% 
+  write_csv("results/beta_core_all_cutoffs.csv")
+
+#ps_Glo_core  <- phylosmith::taxa_filter (ps_E2, treatment = "PlaSpe", frequency =0.6, subset= "HolLan")
+
+#ps_core_PlaSpe  <- merge_samples(ps_Glo_core, group = "PlantSpeciesfull", fun = sum)
 
 #calculate beta diversity as percentage 
-cores  <- 
-  as.data.frame (otu_table (ps_core_PlaSpe )) %>%   as_tibble(rownames = "PlantSpeciesfull") %>% 
-  pivot_longer (!PlantSpeciesfull, names_to="ASV_ID", values_to  = "ASV_counts")  %>%  
-  group_by (PlantSpeciesfull) %>%  
-  dplyr::filter (ASV_counts != "0")  %>% 
-  dplyr::tally(name="core") %>% 
-  left_join(uniqueASVsperPlaSpe)  %>% 
-  mutate (perc_core = 100 -(100* core/uniqueASVsperPlSpe)) %>% 
-  mutate (perc_core = round (perc_core,1) ) %>% 
-  select (PlantSpeciesfull, perc_core)
+# cores  <- 
+#   as.data.frame (otu_table (ps_core_PlaSpe )) %>%   as_tibble(rownames = "PlantSpeciesfull") %>% 
+#   pivot_longer (!PlantSpeciesfull, names_to="ASV_ID", values_to  = "ASV_counts")  %>%  
+#   group_by (PlantSpeciesfull) %>%  
+#   dplyr::filter (ASV_counts != "0")  %>% 
+#   dplyr::tally(name="core") %>% 
+#   left_join(uniqueASVsperPlaSpe)  %>% 
+#   mutate (perc_core = 100 -(100* core/uniqueASVsperPlSpe)) %>% 
+#   mutate (perc_core = round (perc_core,1) ) %>% 
+#   select (PlantSpeciesfull, perc_core)
 
 
 ## MPD ####
@@ -150,6 +255,15 @@ comm_df_Glo_PlaSpe  <- data.frame (otu_table (ps_Glo_perPlaSpe)) # vegan expects
 
 ses.MPD_Glo_PlaSpe  <-  as_tibble (ses.mpd (comm_df_Glo_PlaSpe, dist_Glo, null.model = "independentswap" ), rownames = "sampleID")# tree is the same as before
 
+# Unifrac #####
+
+Unifrac_E1 <- 
+  ps_E1 %>%  phyloseq_sep_variable("PlantSpeciesfull") %>% 
+  map (~phyloseq::distance(., method = "uunifrac", type = "samples")) %>% 
+  map (~mean (.)) %>% 
+  as_tibble () %>% 
+  add_column (Exp = "E1") %>% 
+  pivot_longer(!Exp ,  values_to = "Unifrac", names_to = "PlantSpeciesfull")
 
 # Table of all diversity metrics ####
 # All metrics are put into one table and the mean per plant species is calculated ##
@@ -162,7 +276,8 @@ All_metrics_E1 <-
   select (!sampleID) %>% 
   unique()  %>%  
   left_join (comp_units %>% select (!repl) %>% select (!"1-CU")) %>%   #includes CU and unique and richness
-  left_join(cores) %>% 
+  left_join(beta_core_E1_3 %>% select (!n)) %>% 
+  left_join(Unifrac_E1) %>% 
   select (!c(PlantType,  unique))  %>% 
   left_join (ses.MPD_Glo  %>%  
                left_join (metaM0)  %>% 
@@ -172,11 +287,10 @@ All_metrics_E1 <-
                select (PlantSpeciesfull,   mean_mpd, -PlantFamily)   %>% 
                unique ())  %>% 
   left_join (ses.MPD_Glo_PlaSpe %>%  select (sampleID, mpd.obs), by = c("PlantSpeciesfull" = "sampleID")) %>% 
-  dplyr::rename("Richness S"= "mean_n_ASV_per_species","Shannon's H'" = "Shannon", "β(CU)" = "CUnits", "β(core)"  = "perc_core",
-                 "MPD" = "mean_mpd", "γ-diversity" = "uniqueASVsperPlSpe", "Phyl. γ-diversity" = "mpd.obs" )  %>% 
-  add_column (Exp = "E1")
+  dplyr::rename("Richness S"= "mean_n_ASV_per_species","Shannon's H'" = "Shannon", "β(CU)" = "CUnits", "β(core)"  = "beta_core3",
+                 "MPD" = "mean_mpd", "γ-diversity" = "uniqueASVsperPlSpe", "Phyl. γ-diversity" = "mpd.obs" ) 
 
-
+write_csv(All_metrics_E1, "results/20240501_All_metrics_E1.csv")
 
 # dataframe of the table #
 All_metrics_E1_df <- 
@@ -209,13 +323,15 @@ All_metrics_E1_samples <-
   group_by(PlantSpeciesfull, PlaSpe)  %>% 
   select (sampleID,  Shannon, Observed) %>% 
   left_join (comp_units %>% select (!c(repl,mean_n_ASV_per_species )) %>% select (!"1-CU")) %>%   #includes CU and unique and richness
-  left_join(cores) %>% 
+  left_join(beta_core_E1_3 %>% select (!n)) %>% 
+  left_join(Unifrac_E1) %>% 
   select (!c(PlantType,  unique))  %>% 
   left_join (ses.MPD_Glo %>%  select (sampleID, mpd.obs) )  %>% 
   left_join (ses.MPD_Glo_PlaSpe %>%  select (sampleID, mpd.obs), by = c("PlantSpeciesfull" = "sampleID")) %>% 
-  dplyr::rename("Richness S" = "Observed","Shannon's H'" = "Shannon", "β(CU)" = "CUnits",  "β(core)"  = "perc_core",
-                "γ-diversity" = "uniqueASVsperPlSpe", "MPD" = "mpd.obs.x",   "Phyl. γ-diversity" = "mpd.obs.y") %>% 
-  add_column (Exp = "E1") 
+  dplyr::rename("Richness S" = "Observed","Shannon's H'" = "Shannon", "β(CU)" = "CUnits",  "β(core)"  = "beta_core3",
+                "γ-diversity" = "uniqueASVsperPlSpe", "MPD" = "mpd.obs.x",   "Phyl. γ-diversity" = "mpd.obs.y") 
+  
+write_csv (All_metrics_E1_samples, "results/20240501_All_metrics_E1_sample.csv")
 
 
 
@@ -282,23 +398,121 @@ comp_units_M1   <- meannumberASVsper_species_M1  %>%
 
 ## Cores####
 
-ps_test <- ps_E2  # make a new ps to change its sample_data to get taxa_core and conglomerate_samples to work
-sample_data (ps_test)  <- sample_data (ps_E2) %>% data.frame () %>%  select (PlantSpeciesfull, PlaSpe) %>%  sample_data()
-ps_focal_roots_M1_core  <- phylosmith::taxa_core (ps_test, treatment =  "PlantSpeciesfull", frequency =0.6)#see above
-ps_focal_roots_M1_core <- phylosmith::conglomerate_samples(ps_focal_roots_M1_core, treatment = "PlantSpeciesfull")
+# ps_test <- ps_E2  # make a new ps to change its sample_data to get taxa_core and conglomerate_samples to work
+# sample_data (ps_test)  <- sample_data (ps_E2) %>% data.frame () %>%  select (PlantSpeciesfull) %>%  sample_data()
+# ps_focal_roots_M1_core  <- 
+#   phylosmith::taxa_core (ps_test, treatment =  "PlantSpeciesfull", 
+#                          frequency =0.6) #see above
+# ps_focal_roots_M1_core <- 
+#   phylosmith::conglomerate_samples(ps_focal_roots_M1_core, treatment = "PlantSpeciesfull")  %>%  
+#     prune_taxa( taxa_sums (.)>0, .) %>% prune_samples(sample_sums (.) > 0, .)
+
+data_E2_melted <- melt_phyloseq(ps_E2)
+
+ASVs_in_1_samples <-
+  data_E2_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>% 
+  filter (n>=1) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+
+ASVs_in_2_samples <-
+  data_E2_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>% 
+  filter (n>=2) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+
+ASVs_in_3_samples <-
+  data_E2_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>%   
+  filter (n>=3) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+ASVs_in_4_samples <-
+  data_E2_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>% 
+  left_join (replicates_samples_after_Glo) %>% 
+  filter (n>=4) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+ASVs_in_5_samples <-
+  data_E2_melted %>% 
+  as_tibble () %>% 
+  filter (Abundance >0) %>% 
+  group_by (PlantSpeciesfull, OTU) %>% 
+  count () %>%   
+  left_join (replicates_samples_after_Glo) %>% 
+  filter (n>=5) %>%  ## Change here 
+  select (PlantSpeciesfull, n) %>% 
+  group_by (PlantSpeciesfull) %>% 
+  count ()
+
+beta_core_E2_1 <- 
+  unique_M1 %>% 
+  left_join (ASVs_in_1_samples) %>% 
+  mutate (beta_core1 = round (1-(n/uniqueASVsperPlSpe),2))
+
+beta_core_E2_2 <- 
+  unique_M1 %>% 
+  left_join (ASVs_in_2_samples) %>% 
+  mutate (beta_core2 =round ( 1-(n/uniqueASVsperPlSpe), 2))
+
+beta_core_E2_3 <- 
+  unique_M1 %>% 
+  left_join (ASVs_in_3_samples) %>% 
+  mutate (beta_core3 = round (1-(n/uniqueASVsperPlSpe), 2))
+
+beta_core_E2_4 <- 
+  unique_M1 %>% 
+  left_join (ASVs_in_4_samples) %>% 
+  mutate (beta_core4 = round (1-(n/uniqueASVsperPlSpe),2))
+
+beta_core_E2_5 <- 
+  unique_M1 %>% 
+  left_join (ASVs_in_5_samples) %>% 
+  mutate (beta_core5 = round (1-(n/uniqueASVsperPlSpe),2))
+
+beta_core_E2_1 %>% 
+  left_join(beta_core_E2_2 %>%  select ( PlantSpeciesfull, beta_core2)) %>% 
+  left_join(beta_core_E2_3 %>%  select ( PlantSpeciesfull, beta_core3 )) %>% 
+  left_join(beta_core_E2_4 %>%  select ( PlantSpeciesfull, beta_core4)) %>% 
+  left_join(beta_core_E2_5 %>%  select ( PlantSpeciesfull, beta_core5)) %>% 
+  write_csv("results/beta_core_all_cutoffs_E2.csv")
+
 
 #calculate beta diversity as percentage 
-cores_roots_M1  <- 
-  as.data.frame (otu_table (ps_focal_roots_M1_core )) %>%   as_tibble(rownames = "ASV_ID") %>% 
-  gather (!ASV_ID, key=PlantSpeciesfull, value = ASV_counts)  %>%  
-  group_by (PlantSpeciesfull) %>%  
-  dplyr::filter (ASV_counts != "0")  %>% 
-  dplyr::tally(name="core") %>% 
-  left_join(unique_M1, by = "PlantSpeciesfull")  %>% 
-  mutate (perc_core = 100- (100* core/uniqueASVsperPlSpe)) %>% 
-  mutate (perc_core = round (perc_core,1) ) %>% 
-  select (PlantSpeciesfull, perc_core) 
-
+# cores_roots_M1  <- 
+#   as.data.frame (otu_table (ps_focal_roots_M1_core )) %>%   as_tibble(rownames = "ASV_ID") %>% 
+#   gather (!ASV_ID, key=PlantSpeciesfull, value = ASV_counts)  %>%  
+#   group_by (PlantSpeciesfull) %>%  
+#   dplyr::filter (ASV_counts != "0")  %>% 
+#   dplyr::tally(name="core") %>% 
+#   left_join(unique_M1, by = "PlantSpeciesfull")  %>% 
+#   mutate (perc_core = 100- (100* core/uniqueASVsperPlSpe)) %>% 
+#   mutate (perc_core = round (perc_core,1) ) %>% 
+#   select (PlantSpeciesfull, perc_core) 
+# 
 
 ## MPD ####
 
@@ -317,6 +531,20 @@ comm_df_Glo_PlaSpe_M1  <- data.frame (otu_table (ps_Glo_perPlaSpe_M1)) # vegan e
 
 ses.MPD_Glo_PlaSpe_M1  <-  as_tibble (ses.mpd (comm_df_Glo_PlaSpe_M1, dist_Glo_M1, null.model = "independentswap" ), rownames = "sampleID")# tree is the same as before
 
+## Unifrac #####
+## 
+Unifrac_E2 <- 
+  ps_E2 %>%  phyloseq_sep_variable("PlantSpeciesfull") %>% 
+  map (~phyloseq::distance(., method = "uunifrac", type = "samples")) %>% 
+  map (~mean (.)) %>% 
+  as_tibble () %>% 
+  add_column (Exp = "E2") %>% 
+  pivot_longer(!Exp ,  values_to = "Unifrac", names_to = "PlantSpeciesfull")
+
+
+
+
+
 
 # All metrics E2  ####
 #Get values for all metrics per plant species in one tibble
@@ -329,7 +557,8 @@ All_metrics_E2 <-
   select (!sampleID) %>% 
   unique()  %>%  
   left_join (comp_units_M1 %>%  select (!'1-CU'))    %>%   #includes CU and unique and richness
-  left_join(cores_roots_M1) %>% 
+  left_join(beta_core_E2_3%>% select (!n)) %>% 
+  left_join (Unifrac_E2) %>% 
   left_join (ses.MPD_Glo_M1  %>%  
                left_join (meta_M1) %>% 
                drop_na ()  %>% 
@@ -338,9 +567,8 @@ All_metrics_E2 <-
                select (PlantSpeciesfull,   mean_mpd, -PlantFamily)   %>% 
                unique ()) %>% 
   left_join (ses.MPD_Glo_PlaSpe_M1 %>%  select (sampleID, mpd.obs), by = c("PlantSpeciesfull" = "sampleID")) %>% 
-  dplyr::rename("Richness S"= "meanASV","Shannon's H'" = "Shannon", "β(CU)" = "CUnits", "β(core)"  = "perc_core",
-                "MPD" = "mean_mpd", "γ-diversity" = "uniqueASVsperPlSpe", "Phyl. γ-diversity" = "mpd.obs" )  %>% 
-add_column (Exp = "E2")
+  dplyr::rename("Richness S"= "meanASV","Shannon's H'" = "Shannon", "β(CU)" = "CUnits", "β(core)"  = "beta_core3",
+                "MPD" = "mean_mpd", "γ-diversity" = "uniqueASVsperPlSpe", "Phyl. γ-diversity" = "mpd.obs" )  
 
 
 # dataframe of table #
@@ -355,15 +583,15 @@ rownames(All_metrics_E2_df)  <- All_metrics_E2_df$PlantSpeciesfull
 All_Metrics_E2_sample  <- 
   adiv_richness_M1 %>% 
   left_join (comp_units_M1 %>%  select (!'1-CU'))    %>%   #includes CU and unique and richness
-  left_join(cores_roots_M1) %>% 
+  left_join(beta_core_E2_3 %>% select (!n)) %>% 
+  left_join(Unifrac_E2) %>% 
   left_join (ses.MPD_Glo_M1 %>%  select (sampleID, mpd.obs) )  %>% 
   select (!meanASV) %>% 
   left_join (ses.MPD_Glo_PlaSpe_M1 %>%  select (sampleID, mpd.obs), by = c("PlantSpeciesfull" = "sampleID")) %>% 
-  dplyr::rename("Richness S" = "Observed","Shannon's H'" = "Shannon", "β(CU)" = "CUnits",  "β(core)"  = "perc_core",
-                "γ-diversity" = "uniqueASVsperPlSpe", "MPD" = "mpd.obs.x",   "Phyl. γ-diversity" = "mpd.obs.y") %>% 
-  add_column (Exp = "E2")
+  dplyr::rename("Richness S" = "Observed","Shannon's H'" = "Shannon", "β(CU)" = "CUnits",  "β(core)"  = "beta_core3",
+                "γ-diversity" = "uniqueASVsperPlSpe", "MPD" = "mpd.obs.x",   "Phyl. γ-diversity" = "mpd.obs.y") 
 
-saveRDS(All_Metrics_E2_sample, "results/All_metrics_E2_sample.rds")
+saveRDS(All_Metrics_E2_sample, "results/20240501_All_metrics_E2_sample.rds")
 
 
 ### Tree for the plant species using two genes: matK and trnL #####
@@ -439,3 +667,6 @@ ses.MPD_Glo_M1 %>%
   select (-runs) %>% write_excel_csv("results/mpd_E2.csv")
 
 
+All_Metrics_E2_sample %>% ggplot (aes (x= PlantSpeciesfull, y = `Richness S`))  + geom_boxplot () +coord_flip()
+
+All_Metrics_E2_sample %>% ggplot (aes (x= PlantSpeciesfull, y = MPD))  + geom_boxplot () +coord_flip()
